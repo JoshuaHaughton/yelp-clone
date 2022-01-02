a89adc03c913d60b7a48ccb81ffcc2096ed39fc9
Joshua Haughton <itsjoshuahaughton@gmail.com>
2022-01-02 01:16:59 +0000

    Connected express server to postgre database, created route to test connection to database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const db = require('./db')
 const PORT = process.env.PORT;
 
+
+app.use(express.json())
+
+app.get('/get', async (req, res) => {
+  const output = await db.query('select * from restaurants');
+  console.log(output);
+  res.send('object');
+})
 
 
 app.listen(PORT, () => {
