f47a4a7a0ecf447d5ae8b5b6be3d1b233ef546d8
Joshua Haughton <itsjoshuahaughton@gmail.com>
2022-01-02 19:56:12 +0000

    Used parameterized query instead of template literal to prevent against SQL injection inside of database query

--- server/server.js
+++ server/server.js
@@ -7,10 +7,49 @@ const PORT = process.env.PORT;
 
 app.use(express.json())
 
-app.get('/get', async (req, res) => {
-  const output = await db.query('select * from restaurants');
-  console.log(output);
-  res.send('object');
+app.get('/api/restaurants/', async (req, res) => {
+
+  try {
+    const results = await db.query('select * from restaurants');
+    console.log(results.rows);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows
+      }
+    })
+    res.send('object');
+
+  } catch(err) {
+    res.status(500).send;
+  }
+
+})
+
+app.get('/api/restaurants/:restaurantId', async (req, res) => {
+  try {
+    const id = req.params.restaurantId;
+    const results = await db.query(`
+    SELECT * FROM restaurants
+    WHERE id = $1`, [id]);
+    console.log(results.rows[0]);
+    console.log(req.params.restaurantId);
+    res.send('object');
+
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows[0]
+      }
+    })
+    // req.restaurantId = results.rows[0]
+    //next()
+    
+  } catch(err) {
+    res.status(500).send;
+  }
 })
 
 
