df6c1f1224205252f290eb3a44f70ef79fa75f7a
Joshua Haughton <itsjoshuahaughton@gmail.com>
2022-01-03 14:51:25 +0000

    Created basic React routing for a Home page, a Restaurant Detail Page, and an Update Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './routes/Home';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
+import UpdatePage from './routes/UpdatePage';
 
 const App = () => {
 
-  return <div>App</div>
+  return <div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+        <Route exact path='/restaurants/:restaurantId/update' element={<UpdatePage />}/>
+        <Route exact path='/restaurants/:restaurantId' element={<RestaurantDetailPage />}/>
+      </Routes>
+    </Router>
+  </div>
 
 }
 
