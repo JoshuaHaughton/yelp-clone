049aab81c6f253bfc69cf7fc79238eb3ec791499
Joshua Haughton <itsjoshuahaughton@gmail.com>
2022-01-03 00:46:46 +0000

    Added a post route to test Restaurant Creation

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const db = require('./db')
 const PORT = process.env.PORT;
 
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/api/restaurants/', async (req, res) => {
 
   try {
     const results = await db.query('select * from restaurants');
-    console.log(results.rows);
+
     res.status(200).json({
       status: "success",
       results: results.rows.length,
@@ -19,23 +19,27 @@ app.get('/api/restaurants/', async (req, res) => {
         restaurants: results.rows
       }
     })
-    res.send('object');
+
+    res.send(results.rows);
 
   } catch(err) {
+
     res.status(500).send;
+
   }
 
 })
 
 app.get('/api/restaurants/:restaurantId', async (req, res) => {
   try {
+
     const id = req.params.restaurantId;
+
     const results = await db.query(`
     SELECT * FROM restaurants
     WHERE id = $1`, [id]);
-    console.log(results.rows[0]);
-    console.log(req.params.restaurantId);
-    res.send('object');
+
+    res.send(results.rows[0]);
 
     res.status(200).json({
       status: "success",
@@ -46,11 +50,38 @@ app.get('/api/restaurants/:restaurantId', async (req, res) => {
     })
     // req.restaurantId = results.rows[0]
     //next()
-    
   } catch(err) {
+
     res.status(500).send;
+
   }
 })
+
+app.post('/api/restaurants', async (req, res) => {
+  console.log(req.body);
+
+  try{
+
+    const results = await db.query(`
+    INSERT INTO restaurants (name, location, price_range)
+    VALUES ($1, $2, $3)
+    RETURNING *`, [req.body.name, req.body.location, req.body.price_range])
+
+    console.log(results.rows[0]);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0]
+      }
+    })
+
+  } catch(err) {
+    res.status(500).send();
+  }
+})
+
+
 
 
 app.listen(PORT, () => {
