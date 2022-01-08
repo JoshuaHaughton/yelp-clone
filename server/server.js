aa4b0b4f361d1646e3663b86d285b736ada5f549
Joshua Haughton <itsjoshuahaughton@gmail.com>
2022-01-08 19:54:40 +0000

    Set up Axios to retrieve data from the SQL database and save it in the RestaurantsContext

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require("cors")
 const app = express();
 const db = require('./db')
 const PORT = process.env.PORT;
 
-
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/restaurants/', async (req, res) => {
