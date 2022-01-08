e4134e0316266ec9a9a6abe49cf7aa288095c347
Joshua Haughton <itsjoshuahaughton@gmail.com>
2022-01-08 18:51:52 +0000

    Context API to store results until we start fetching data from backend server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 import Home from './routes/Home';
 import RestaurantDetailPage from './routes/RestaurantDetailPage';
 import UpdatePage from './routes/UpdatePage';
 
 const App = () => {
+  
+  return ( 
+  
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route exact path='/' element={<Home />}/>
+            <Route exact path='/restaurants/:restaurantId/update' element={<UpdatePage />}/>
+            <Route exact path='/restaurants/:restaurantId' element={<RestaurantDetailPage />}/>
+          </Routes>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
+  
+  
 
-  return <div>
-    <Router>
-      <Routes>
-        <Route exact path='/' element={<Home />}/>
-        <Route exact path='/restaurants/:restaurantId/update' element={<UpdatePage />}/>
-        <Route exact path='/restaurants/:restaurantId' element={<RestaurantDetailPage />}/>
-      </Routes>
-    </Router>
-  </div>
+  )
 
 }
 
